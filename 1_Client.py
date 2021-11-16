0975a467f8479049ce05fc87bbe988112ce63add
Mickey86R <andreychenkome@gmail.com>
2021-11-17 04:36:03 +0500

    Add files via upload

---
+++ 1_Client.py
@@ -0,0 +1,25 @@
+import socket
+import time
+
+IP = "10.0.0.113"
+PORT = 7800
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+while True:
+    client.connect((IP, PORT))
+    data = client.recv(2048)
+    print(data.decode("UTF-16"))
+    data = input("::")
+    while (data != "quit"):
+        client.send(data.encode("UTF-16"))
+        data = input("::")
+    client.send(data.encode("UTF-16"))
+    break
+
+client.close()
+
+
+
+
+#python C:\Users\Anony\Documents\Client.py
---
+++ 1_Server.py
@@ -0,0 +1,29 @@
+import socket
+import time
+
+IP = "10.0.0.113"
+PORT = 7800
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+
+server.listen()
+
+while True:
+    sock, addr = server.accept()
+    print(f"К серверу подключился {sock}")
+    sock.send("Welcome to Server!".encode("UTF-16"))
+    data = sock.recv(2048)
+    while (data.decode("UTF-16") != "quit"):
+        print(data.decode("UTF-16"))
+        data = sock.recv(2048)
+    print(data.decode("UTF-16") + "qweqwe")
+    break
+
+server.close()
+print("Сервер закрыт")
+
+
+
+
+#python C:\Users\Anony\Documents\Server.py
